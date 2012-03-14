c22936054a37817387b7d9cd27d329da84eb280d
Toshiyuki HIRANO <hiracchi@gmail.com>
2012-03-14 19:04:20 +0900

    use color-theme-ir-black

--- dot.emacs.d/elisp/color-theme-ir-black.el
+++ dot.emacs.d/elisp/color-theme-ir-black.el
@@ -1,65 +1,80 @@
-
-;; -*- emacs-lisp -*-
-;;
-;;
-;; name: color-theme-ir-black
-;; date: Sat Sep 25 2010 20:54:37 GMT+0200 (CET)
-;;
-;;
-;; To use this theme save this code into a
-;; file named color-theme-ir-black.el and place it
-;; in a directory in your load-path
-;;
-;;    (require 'color-theme-ir-black)
-;;    (color-theme-ir-black)
-;;
-
+;;; color-theme-ir-black.el --- pastel color theme 
+;; MIT License Copyright (c) 2009 Burke Libbey <burke@burkelibbey.org>
+;; URL: https://github.com/kurtharriger/color-theme-ir-black
+;; Author: Burke Libbey, Kurt Harriger
+;; Version: 1.0.1
+;; Package-Requires: ((color-theme "6.6.1"))
 
 (require 'color-theme)
-
 (defun color-theme-ir-black ()
-  "Generated with http://color-theme-select.heroku.com/ on Sat Sep 25 2010 20:54:36 GMT+0200 (CET)  
-color-theme-ir-black"
   (interactive)
-  (color-theme-install
-    '(color-theme-ir-black
-      (
-       (background-color . "#000000")
-       (background-mode . "dark")
-       (border-color . "#000000")
-       (cursor-color . "#FFA560")
-       (foreground-color . "#F6F3E8")
-       (mouse-color . "sienna1")
-      )
-      (
-      )
-      (default  ((t (:foreground "#F6F3E8" :background "#000000"))))
-      (blue  ((t (:foreground "blue"))))
-      (buffers-tab  ((t (:foreground "#F6F3E8" :background "#000000"))))
-      (font-lock-builtin-face  ((t (:foreground "#F6F3E8"))))
-      (font-lock-comment-face  ((t (:foreground "#7C7C7C"))))
-      (font-lock-constant-face  ((t (:foreground "#99CC99"))))
-      (font-lock-doc-string-face  ((t (:foreground "#A8FF60"))))
-      (font-lock-function-name-face  ((t (:foreground "#FFB774"))))
-      (font-lock-keyword-face  ((t (:foreground "#66B5FF"))))
-      (font-lock-preprocessor-face  ((t (:foreground "#66B5FF"))))
-      (font-lock-reference-face  ((t (:foreground "#99CC99"))))
-      (font-lock-regexp-grouping-backslash-face  ((t (:foreground "#FF6C60"))))
-      (font-lock-regexp-grouping-construct-face  ((t (:foreground "#FF6C60"))))
-      (font-lock-string-face  ((t (:foreground "#A8FF60"))))
-      (font-lock-type-face  ((t (:foreground "#FFB774"))))
-      (font-lock-variable-name-face  ((t (:foreground "#C6C5FE"))))
-      (font-lock-warning-face  ((t (:foreground "white" :background "#FF6C60"))))
-      (gui-element  ((t (:foreground "#000000" :background "#D4D0C8"))))
-      (region  ((t (:background "#1D1E2C"))))
-      (mode-line  ((t (:foreground "#000000" :background "grey75"))))
-      (highlight  ((t (:background "#151515"))))
-      (highline-face  ((t (:background "#151515"))))
-      (text-cursor  ((t (:foreground "#000000" :background "yellow"))))
-      (bold  ((t (:bold t))))
-      (bold-italic  ((t (:bold t))))
-     )
-  )
-)
+
+  (let ((*normal*       "#F6F3E8")
+        (*background*   "#000000")
+        (*cursor*       "#FFA560")
+        (*mouse*        "sienna1")
+        (*region*       "#1D1E2C")
+        (*current-line* "#151515")
+        (*string*       "#A8FF60")
+        (*keyword*      "#66B5FF")
+        (*method*       "#FFB774")
+        (*comment*      "#7C7C7C")
+        (*constant*     "#99CC99")
+        (*red*          "#FF6C60")
+        (*operator*     "#FFFFFF")
+        (*class*        "#FFFFB6")
+        (*variable*     "#C6C5FE"))
+
+    (flet ((color (fgcolor &optional (bgcolor nil) (bold nil) (italic nil) (underline nil))
+                  `((t (,@(if fgcolor   `(:foreground ,fgcolor))
+                        ,@(if bgcolor   `(:background ,bgcolor))
+                        ,@(if bold      '(:bold       t))
+                        ,@(if italic    '(:italic     t))
+                        ,@(if underline '(:underline  t))))))
+           (face (face &rest args)
+                 `(,(intern (concat "font-lock-" face "-face"))
+                   ,(apply #'color args))))
+
+      (color-theme-install
+       `(color-theme-ir-black
+         ((background-color . ,*background*)
+          (background-mode  . dark)
+          (border-color     . ,*background*)
+          (cursor-color     . ,*cursor*)
+          (foreground-color . ,*normal*)
+          (mouse-color      . ,*mouse*))
+         (default ,(color *normal* *background*))
+         (blue ,(color "blue"))
+         (border-glyph ((t (nil))))
+         (buffers-tab ,(color *normal* *background*))
+         ,(face "builtin" *normal*)
+         ,(face "comment" *comment*)
+         ,(face "constant" *constant*)
+         ,(face "doc-string" *string*)
+         ,(face "function-name" *method*)
+         ,(face "keyword" *keyword*)
+         ,(face "preprocessor" *keyword*)
+         ,(face "reference" "#99CC99")
+         ,(face "regexp-grouping-backslash" *red*)
+         ,(face "regexp-grouping-construct" *red*)
+         ,(face "string" *string*)
+         ,(face "type" "#FFB774")
+         ,(face "variable-name" *variable*)
+         ,(face "warning" "white" *red*)
+         (gui-element ,(color *background* "#D4D0C8"))
+         (region ,(color nil *region*))
+         (mode-line ,(color *background* "grey75"))
+         (highlight ,(color nil *current-line*))
+         (highline-face ,(color nil *current-line*))
+         (italic ((t (nil))))
+         (left-margin ((t (nil))))
+         (text-cursor ,(color *background* "yellow"))
+         (toolbar ((t (nil))))
+         (bold ((t (:bold t))))
+         (bold-italic ((t (:bold t))))
+         (underline ((nil (:underline nil)))))))))
 
 (provide 'color-theme-ir-black)
+
+;;; color-theme-ir-black.el ends here
+
--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -1,7 +1,10 @@
+;; for debug mode ==============================================================
 (require 'cl)
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq inhibit-startup-screen t)
 
+
+;; emacs setting and management ================================================
 (when (> emacs-major-version 23)
   (defvar user-emacs-directory "~/.emacs.d"))
 
@@ -13,16 +16,230 @@
         (add-to-list 'load-path default-directory)
         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
             (normal-top-level-add-subdirs-to-load-path))))))
+
 (add-to-load-path "elisp" "conf" "public_repos")
+
+
+;; for machine environment =====================================================
+;; not for terminal
+(when window-system
+  ;; disable tool-bar
+  (tool-bar-mode 0)
+  ;; disable scroll-bar
+  (scroll-bar-mode 0))
+
+;; not for CocoaEmacs
+(unless (eq window-system 'ns)
+  ;; disable menu-bar
+  (menu-bar-mode 0))
+
+
+;; key-bind ====================================================================
+;; assign backspace to C-h
+(keyboard-translate ?\C-h ?\C-?)
+;; assign C-m to newline-and-indent
+(global-set-key (kbd "C-m") 'newline-and-indent)
+;; 
+(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
+;; window switch / default "C-t" is transpose-chars
+(define-key global-map (kbd "C-t") 'other-window)
+
+
+;; environment variable ========================================================
+(add-to-list 'exec-path "/usr/local/bin")
+(add-to-list 'exec-path "~/bin")
+
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+
+  ;; for mac
+(when (eq system-type 'darwin)
+  ;; for filename
+  (require 'ucs-normalize)
+  (set-file-name-coding-system 'utf-8-hfs)
+  (setq locale-coding-system 'utf-8-hfs))
+
+  ;; for windows
+(when (eq window-system 'w32)
+  (set-file-name-coding-system 'cp932)
+  (setq locale-coding-system 'cp932))
+
+
+;; frame =======================================================================
+;; show column number
+(column-number-mode t)
+;; show file size
+(size-indication-mode t)
+;; time, day and date
+(setq display-time-day-and-date t)
+(setq display-time-24ht-format t)
+(display-time-mode t)
+;; battery
+(display-battery-mode t)
+;; the number of lines and chars in the region
+;; http://d.hatena.ne.jp/sonota88/20110224/1298557375
+(defun count-lines-and-chars ()
+  (if mark-active
+      (format "%d lines, %d chars "
+	      (count-lines (region-beginning) (region-end))
+	      (- (region-end) (region-beginning)))
+    ;; flicker on the below case
+    ;;(count-lines-region (region-beginning) (region-end))
+    ""))
+(add-to-list 'default-mode-line-format
+             '(:eval (count-lines-and-chars)))
+;; show pullpath
+(setq frame-title-format "%f")
+;; line number
+;; (global-linum-mode t)
+
+
+;; indent ======================================================================
+;; TAB
+(setq-default tab-width 4)
+;; don't show TAB char in the case of indent
+(setq-default indent-tabs-mode nil)
+;; NOT use TAB in php-mode
+;; (add-hook 'php-mode-hook
+;;           '(lambda ()
+;;             (setq indent-tabs-mode nil)))
+
+;; indent for C、C++、JAVA、and PHP etc.
+(add-hook 'c-mode-common-hook
+          '(lambda ()
+             (c-set-style "bsd")))
+
+;; display, decolation =========================================================
+;; region background
+;; (set-face-background 'region "darkgreen")
+;; color-theme (http://download.savannah.gnu.org/releases/color-theme/color-theme-6.6.0.tar.gz)
+(when (require 'color-theme nil t)
+  (color-theme-initialize)
+  ;(color-theme-ramangalahy)
+  ;(color-theme-blue-sea)
+)
+(require 'color-theme-ir-black)
+;(load "color-theme-ir-black")
+(color-theme-ir-black)
+;(color-theme-ir-black)
+;; font
+(when (eq window-system 'ns)
+  ;; asciiフォントをMenloに
+  (set-face-attribute 'default nil
+                      :family "Menlo"
+                      :height 120)
+  ;; 日本語フォントをヒラギノ明朝 Proに
+  (set-fontset-font
+   nil 'japanese-jisx0208
+   ;; 英語名の場合
+   ;; (font-spec :family "Hiragino Mincho Pro"))
+   (font-spec :family "ヒラギノ明朝 Pro"))
+  ;; ひらがなとカタカナをモトヤシーダに
+  ;; U+3000-303F	CJKの記号および句読点
+  ;; U+3040-309F	ひらがな
+  ;; U+30A0-30FF	カタカナ
+  (set-fontset-font
+   nil '(#x3040 . #x30ff)
+   (font-spec :family "NfMotoyaCedar"))
+  ;; フォントの横幅を調節する
+  (setq face-font-rescale-alist
+        '((".*Menlo.*" . 1.0)
+          (".*Hiragino_Mincho_Pro.*" . 1.2)
+          (".*nfmotoyacedar-bold.*" . 1.2)
+          (".*nfmotoyacedar-medium.*" . 1.2)
+          ("-cdac$" . 1.3))))
+
+(when (eq system-type 'windows-nt)
+  ;; asciiフォントをConsolasに
+  (set-face-attribute 'default nil
+                      :family "Consolas"
+                      :height 120)
+  ;; 日本語フォントをメイリオに
+  (set-fontset-font
+   nil
+   'japanese-jisx0208
+   (font-spec :family "メイリオ"))
+  ;; フォントの横幅を調節する
+  (setq face-font-rescale-alist
+        '((".*Consolas.*" . 1.0)
+          (".*メイリオ.*" . 1.15)
+          ("-cdac$" . 1.3))))
+
+
+;; high-light ==================================================================
+(defface my-hl-line-face
+  ;; 背景がdarkならば背景色を紺に
+  '((((class color) (background dark))
+     (:background "NavyBlue" t))
+    ;; 背景がlightならば背景色を緑に
+    (((class color) (background light))
+     (:background "LightGoldenrodYellow" t))
+    (t (:bold t)))
+  "hl-line's my face")
+(setq hl-line-face 'my-hl-line-face)
+(global-hl-line-mode t)
+;; paren-mode
+(setq show-paren-delay 0)
+(show-paren-mode t)
+(setq show-paren-style 'expression)
+(set-face-background 'show-paren-match-face nil)
+(set-face-underline-p 'show-paren-match-face "yellow")
+
+
+;; backup ======================================================================
+;;; P102-103 バックアップとオートセーブの設定
+;; バックアップファイルを作成しない
+;; (setq make-backup-files nil) ; 初期値はt
+;; オートセーブファイルを作らない
+;; (setq auto-save-default nil) ; 初期値はt
+
+;; バックアップファイルの作成場所をシステムのTempディレクトリに変更する
+;; (setq backup-directory-alist
+;;       `((".*" . ,temporary-file-directory)))
+;; オートセーブファイルの作成場所をシステムのTempディレクトリに変更する
+;; (setq auto-save-file-name-transforms
+;;       `((".*" ,temporary-file-directory t)))
+;; バックアップとオートセーブファイルを~/.emacs.d/backups/へ集める
+;;(add-to-list 'backup-directory-alist
+;;             (cons "." "~/.emacs.d/backups/"))
+;;(setq auto-save-file-name-transforms
+;;      `((".*" ,(expand-file-name "~/.emacs.d/backups/") t)))
+;; オートセーブファイル作成までの秒間隔
+(setq auto-save-timeout 15)
+;; オートセーブファイル作成までのタイプ間隔
+(setq auto-save-interval 60)
+
+
+;; hook ========================================================================
+;; ファイルが #! から始まる場合、+xを付けて保存する
+(add-hook 'after-save-hook
+          'executable-make-buffer-file-executable-if-script-p)
+;; emacs-lisp-mode-hook用の関数を定義
+(defun elisp-mode-hooks ()
+  "lisp-mode-hooks"
+  (when (require 'eldoc nil t)
+    (setq eldoc-idle-delay 0.2)
+    (setq eldoc-echo-area-use-multiline-p t)
+    (turn-on-eldoc-mode)))
+;; emacs-lisp-modeのフックをセット
+(add-hook 'emacs-lisp-mode-hook 'elisp-mode-hooks)
+
 
 ;; auto-install ================================================================
 (when (require 'auto-install nil t)
   (setq auto-install-directory "~/.emacs.d/elisp/")
   (auto-install-update-emacswiki-package-name t)
   (auto-install-compatibility-setup))
+;;(install-elisp "http://www.emacswiki.org/emacs/download/redo+.el")
+(when (require 'redo+ nil t)
+  ;; C-' にリドゥを割り当てる
+  (global-set-key (kbd "C-'") 'redo)
+  ;; (global-set-key (kbd "C-.") 'redo)
+  )
+
 
 ;; anything ====================================================================
-(when (require 'anything nil t)
+(when (require 'anything-startup nil t)
   (setq 
    anything-idle-delay 0.3
    anything-input-idle-delay 0.2
@@ -52,109 +269,18 @@
 )
 
 ;; auto-complete ==============================================================
-;(add-to-list 'load-path "~/.emacs.d/elisp/")
 (when (require 'auto-complete-config nil t)
   (add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/ac-dict")
   (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
   (ac-config-default)
 )
 
+
 ;; common user access =========================================================
 (cua-mode t)
 (setq cua-enable-cua-keys nil)
 
-;; environment ================================================================
-(add-to-list 'exec-path "/usr/local/bin")
-(add-to-list 'exec-path "~/bin")
-
-(set-language-environment "Japanese")
-(prefer-coding-system 'utf-8)
-
-  ;; for mac
-(when (eq system-type 'darwin)
-  ;; for filename
-  (require 'ucs-normalize)
-  (set-file-name-coding-system 'utf-8-hfs)
-  (setq locale-coding-system 'utf-8-hfs))
-
-  ;; for windows
-(when (eq window-system 'w32)
-  (set-file-name-coding-system 'cp932)
-  (setq locale-coding-system 'cp932))
-
-
-;; visual =====================================================================
-;; NOT for terminal
-(when window-system
-  ;; disable tool-bar
-  (tool-bar-mode 0)
-  ;; disable scroll-bar
-  (scroll-bar-mode 0))
-
-;; NOT for CocoaEmacs
-(unless (eq window-system 'ns)
-  (menu-bar-mode 0))
-
-(column-number-mode t)
-(line-number-mode t)
-
-;; file
-(size-indication-mode t)
-(setq frame-title-format "%f")
-
-;; time, day and date
-(setq display-time-day-and-date t)
-(setq display-time-24ht-format t)
-(display-time-mode t)
-;(display-battery-mode t)
-
-;; http://d.hatena.ne.jp/sonota88/20110224/1298557375
-(defun count-lines-and-chars ()
-  (if mark-active
-    (format "%d lines, %d chars "
-      (count-lines (region-beginning) (region-end))
-      (- (region-end) (region-beginning)))
-    ;; flicker on the below case
-    ;;(count-lines-region (region-beginning) (region-end))
-    ""))
-(add-to-list 'default-mode-line-format
-             '(:eval (count-lines-and-chars)))
-
-;; color-theme
-;(when (require 'color-theme nil t)
-;  (color-theme-initialize)
-;)
-(require 'color-theme-ir-black)
-;(color-theme-irblack)
-;(load-file "~/.emacs.d/elisp/color-theme-ir-black.el")
-;(color-theme-ir-black)
-
-;; hilight current line
-(defface my-hl-line-face
-  '((((class color) (background dark))
-     (:background "NavyBlue" t))
-    (((class color) (background light))
-     (:backgound "LightGoldenrodYellow" t))
-    (t (:bold t)))
-  "hi-line's my face")
-(setq hl-line-face 'my-hl-line-face)
-(global-hl-line-mode t)
-
-;; paren-mode
-(setq show-paren-delay 0)
-(show-paren-mode t)
-(setq show-paren-style 'expression)
-(set-face-background 'show-paren-match-face nil)
-(set-face-underline-p 'show-paren-match-face "yellow")
-
-;; key-bind ====================================================================
-(keyboard-translate ?\C-h ?\C-?)
-(global-set-key (kbd "C-m") 'newline-and-indent)
-(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
-(define-key global-map (kbd "C-t") 'other-window)
-
 ;; programing ==================================================================
-
 ;; file extention
 (setq auto-mode-alist
       (append '(("\\.tpl$" . html-mode)
@@ -168,24 +294,6 @@
                 ("\\.py$"  . python-mode)
                 ("\\.pp$"  . puppet-mode)
                 ) auto-mode-alist))
-
-;; for shebang
-(add-hook 'after-save-hook 'my-chmod-script)
-(defun my-chmod-script() 
-  (interactive) (save-restriction 
-                  (widen)
-                  (let ((name (buffer-file-name)))
-                    (if (and (not (string-match ":" name))
-                             (not (string-match "/\\.[^/]+$" name))
-                             (equal "#!" (buffer-substring 1 (min 3 (point-max)))))
-                        (progn (set-file-modes name (logior (file-modes name) 73))
-                               (message "Wrote %s (chmod +x)" name)
-                               )
-                      )
-                    )
-                  )
-  )
-
 
 ;; for C/C++
 (require 'cc-mode)
@@ -225,4 +333,6 @@
 ;; git
 (when (executable-find "git")
   (require 'egg nil t))
+
+;(line-number-mode t)
 
