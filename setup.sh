90e3c6f9ee95ad97d7296a87f02dbe22af98f2c6
Toshiyuki HIRANO <hiracchi@gmail.com>
2012-10-25 11:52:45 +0900

    Add: oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,16 @@
 SRC_DIR=$(cd $(dirname $0); pwd)
 echo $SRC_DIR
 
+# oh-my-zsh
+if [ ! -d ${HOME}/.oh-my-zsh ]; then
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ${HOME}/.oh-my-zsh
+fi
+
 ln -sf ${SRC_DIR}/dot.zshenv ${HOME}/.zshenv
 ln -sf ${SRC_DIR}/dot.zshrc ${HOME}/.zshrc
+ln -sf ${SRC_DIR}/dot.zshrc_common ${HOME}/.zshrc.common
 ln -sf ${SRC_DIR}/dot.emacs.d ${HOME}/.emacs.d
 ln -sf ${SRC_DIR}/dot.vimrc ${HOME}/.vimrc
 ln -sf ${SRC_DIR}/dot.screenrc ${HOME}/.screenrc
+
 
