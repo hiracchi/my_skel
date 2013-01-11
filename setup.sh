1b28dcd88ee7559cb044190c4b7f14a71dbaa5f8
Toshiyuki HIRANO <hiracchi@gmail.com>
2013-01-11 12:51:20 +0900

    Add: tmux

--- setup.sh
+++ setup.sh
@@ -8,12 +8,14 @@ if [ ! -d ${HOME}/.oh-my-zsh ]; then
     git clone git://github.com/robbyrussell/oh-my-zsh.git ${HOME}/.oh-my-zsh
 fi
 
-ln -sf ${SRC_DIR}/dot.zshenv ${HOME}/.zshenv
-ln -sf ${SRC_DIR}/dot.zshrc ${HOME}/.zshrc
-ln -sf ${SRC_DIR}/dot.zshrc_common ${HOME}/.zshrc.common
-ln -sf ${SRC_DIR}/dot.zlogin ${HOME}/.zlogin
+ln -snf ${SRC_DIR}/dot.zshenv ${HOME}/.zshenv
+ln -snf ${SRC_DIR}/dot.zshrc ${HOME}/.zshrc
+ln -snf ${SRC_DIR}/dot.zshrc_common ${HOME}/.zshrc.common
+ln -snf ${SRC_DIR}/dot.zlogin ${HOME}/.zlogin
 ln -snf ${SRC_DIR}/dot.emacs.d ${HOME}/.emacs.d
-ln -sf ${SRC_DIR}/dot.vimrc ${HOME}/.vimrc
-ln -sf ${SRC_DIR}/dot.screenrc ${HOME}/.screenrc
-ln -sf ${SRC_DIR}/dot.fd2rc ${HOME}/.fd2rc
+ln -snf ${SRC_DIR}/dot.vimrc ${HOME}/.vimrc
+
+ln -snf ${SRC_DIR}/dot.tmux.conf ${HOME}/.tmux.conf
+ln -snf ${SRC_DIR}/dot.screenrc ${HOME}/.screenrc
+ln -snf ${SRC_DIR}/dot.fd2rc ${HOME}/.fd2rc
 
